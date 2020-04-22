a648899aec1f9bac4a83ad0c5da5d1dab4d9dc13
Joshua Joseph Costa <jjcosta@circinus-28.ics.uci.edu>
2020-04-22 16:49:29 -0700

    Setup board

--- src/MyAI.py
+++ src/MyAI.py
@@ -20,58 +20,40 @@ mineTotal = 0
 numOfUncovered = 0
 totalTiles = 0
 
+class Tile:
+	
+	def __init__():
+		self.visited = False
+		self.covered = True
+		self.marked = False
+		self.label = 0
+		self.effectiveLabel = 0
+
 class MyAI( AI ):
 
 	def __init__(self, rowDimension, colDimension, totalMines, startX, startY):
-                self.board = [[0] * (colDimension + 1) for i in range(rowDimension + 1)]
-                totalTiles = rowDimension * colDimension
-                mineTotal = totalMines
-                for i in range(rowDimension, 0, -1):
-                        for j in range(colDimension, 0 , -1):
-                                if i == 1:
-                                        if j == 1:
-                                                self.board[i][j] = {"12" : 0, "22" : 0, "21" : 0}
-                                        elif j == colDimension:
-                                                self.board[i][j] = {"71" : 0, "72" : 0, "82" : 0}
-                                        else:
-                                                self.board[i][j] = {str(i)+str(j - 1): 0, str(i + 1)+str(j - 1): 0, str(i + 1)+str(j): 0, str(i + 1)+str(j + 1): 0, str(i)+str(j + 1): 0}
-                                elif i == colDimension:
-                                        if j == 1:
-                                                self.board[i][j] = {"17" : 0, "27" : 0, "28" : 0}
-                                        elif j == colDimension:
-                                                self.board[i][j] = {"78" : 0, "77" : 0, "87" : 0}
-                                        else:
-                                                self.board[i][j] = {str(i) + str(j - 1): 0, str(i - 1) + str(j - 1): 0, str(i - 1) + str(j): 0, str(i - 1) + str(j + 1): 0, str(i) + str(j + 1): 0}
-                                else:
-                                        if j == 1:
-                                                self.board[i][j] = {str(i - 1) + str(j): 0, str(i - 1) + str(j + 1): 0, str(i) + str(j + 1): 0, str(i + 1) + str(j + 1): 0, str(i + 1) + str(j): 0}
-                                        elif j == colDimension:
-                                                self.board[i][j] = {str(i - 1) + str(j): 0, str(i - 1) + str(j - 1): 0, str(i) + str(j - 1): 0, str(i + 1) + str(j - 1): 0, str(i + 1) + str(j): 0}
-                                        else:
-                                                self.board[i][j] = {str(i) + str(j - 1): 0, str(i + 1) + str(j - 1): 0, str(i + 1) + str(j): 0, str(i + 1) + str(j + 1): 0, str(i) + str(j + 1):0, str(i - 1) + str(j + 1): 0, str(i - 1) + str(j): 0, str(i - 1) + str(j - 1): 0}
+
+		# Set up board array
+                self.board = [[Tile()] * (colDimension) for _ in range(rowDimension)]
+
+		# Set up board vars
+                self.totalTiles = rowDimension * colDimension
+                self.mineTotal = totalMines
+		
+		self.x = startX
+		self.y = startY
 
 
-
-		########################################################################
-		#							YOUR CODE BEGINS						   #
-		########################################################################
-		# col = x & row = y
-		# for dictionary value: 0 ==> covered, 1 ==> uncovered, -1 ==> mine/flag
-
-                
                       
-		########################################################################
-		#							YOUR CODE ENDS							   #
-		########################################################################
 
 		
 	def getAction(self, number: int) -> "Action Object":
-
-		########################################################################
-		#							YOUR CODE BEGINS						   #
-		########################################################################
-		if numOfUncovered == totalTiles:
+	
+		# Calculate the amount of covered tiles	
+		self.coveredTiles = sum([sum([1 for tile in self.board[row] if tile.covered == True]) for row in range(self.rowDimension)])
+	
+		# LEAVE if we meet the game condition	
+		if self.coveredTiles == self.totalTiles:
                         return Action(AI.Action.LEAVE)
-		########################################################################
-		#							YOUR CODE ENDS							   #
-		########################################################################
+
+
