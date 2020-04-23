2a858c4c93b1e2737bf4131371c2c8828cf7a524
Andrew Ha <anha1@circinus-21.ics.uci.edu>
2020-04-22 17:29:36 -0700

    Modified MyAI.py

--- src/MyAI.py
+++ src/MyAI.py
@@ -20,58 +20,70 @@ mineTotal = 0
 numOfUncovered = 0
 totalTiles = 0
 
+class Tile:
+	
+	def __init__(self):
+		self.visited = False
+		self.covered = True
+		self.marked = False
+		self.label = 0
+		self.effectiveLabel = 0
+		self.markedNeighbor = 0
+
 class MyAI( AI ):
 
 	def __init__(self, rowDimension, colDimension, totalMines, startX, startY):
-                self.board = [[0] * (colDimension + 1) for i in range(rowDimension + 1)]
-                totalTiles = rowDimension * colDimension
-                mineTotal = totalMines
-                for i in range(rowDimension, 0, -1):
-                        for j in range(colDimension, 0 , -1):
-                                if i == 1:
-                                        if j == 1:
-                                                self.board[i][j] = {"12" : 0, "22" : 0, "21" : 0}
-                                        elif j == colDimension:
-                                                self.board[i][j] = {"71" : 0, "72" : 0, "82" : 0}
-                                        else:
-                                                self.board[i][j] = {str(i)+str(j - 1): 0, str(i + 1)+str(j - 1): 0, str(i + 1)+str(j): 0, str(i + 1)+str(j + 1): 0, str(i)+str(j + 1): 0}
-                                elif i == colDimension:
-                                        if j == 1:
-                                                self.board[i][j] = {"17" : 0, "27" : 0, "28" : 0}
-                                        elif j == colDimension:
-                                                self.board[i][j] = {"78" : 0, "77" : 0, "87" : 0}
-                                        else:
-                                                self.board[i][j] = {str(i) + str(j - 1): 0, str(i - 1) + str(j - 1): 0, str(i - 1) + str(j): 0, str(i - 1) + str(j + 1): 0, str(i) + str(j + 1): 0}
-                                else:
-                                        if j == 1:
-                                                self.board[i][j] = {str(i - 1) + str(j): 0, str(i - 1) + str(j + 1): 0, str(i) + str(j + 1): 0, str(i + 1) + str(j + 1): 0, str(i + 1) + str(j): 0}
-                                        elif j == colDimension:
-                                                self.board[i][j] = {str(i - 1) + str(j): 0, str(i - 1) + str(j - 1): 0, str(i) + str(j - 1): 0, str(i + 1) + str(j - 1): 0, str(i + 1) + str(j): 0}
-                                        else:
-                                                self.board[i][j] = {str(i) + str(j - 1): 0, str(i + 1) + str(j - 1): 0, str(i + 1) + str(j): 0, str(i + 1) + str(j + 1): 0, str(i) + str(j + 1):0, str(i - 1) + str(j + 1): 0, str(i - 1) + str(j): 0, str(i - 1) + str(j - 1): 0}
+		# Set up board
+		self.board = [[Tile()] * (colDimension) for _ in range(rowDimension)]
 
+		# Set up vars
+		self.totalTiles = rowDimension * colDimension
+		self.mineTotal = totalMines
+		self.rowDimension = rowDimension
+		self.colDimension = colDimension
 
+		self.x = startX
+		self.y = startY
+		self.board[startX][startY].visited = True
+		self.board[startX][startY].covered = False
 
-		########################################################################
-		#							YOUR CODE BEGINS						   #
-		########################################################################
-		# col = x & row = y
-		# for dictionary value: 0 ==> covered, 1 ==> uncovered, -1 ==> mine/flag
-
-                
-                      
-		########################################################################
-		#							YOUR CODE ENDS							   #
-		########################################################################
-
+	def position(self, x, y):
+		return (x < 10 and x > 0 and y < 10 and y > 0)
+	
+	def neighbor(self, x, y):
+		result = list()
+		if self.position(x, y - 1):	# Check for left neighbor
+			result.append((x, y - 1))
+		if self.position(x - 1, y):	# Check for bottom neighbor
+			result.append((x - 1, y))
+		if self.position(x, y + 1):	# Check for right neighbor
+			result.append((x, y + 1))
+		if self.position(x + 1, y):	# Check for top neighbor
+			result.append((x + 1, y))
+		return result
 		
 	def getAction(self, number: int) -> "Action Object":
+		# number = number of mines in the neighborhood
 
-		########################################################################
-		#							YOUR CODE BEGINS						   #
-		########################################################################
-		if numOfUncovered == totalTiles:
+		# Action for the AI
+		action = ""
+		# Calculate the amount of covered tiles	
+		self.coveredTiles = sum([sum([1 for tile in self.board[row] if tile.covered == True]) for row in range(self.rowDimension)])
+
+		result = self.neighbor(self.x, self.y)
+		if number == 0:	# No mines around the neighborhood
+			# Need to check the neighbor before making a move
+			
+			action = AI.Action.UNCOVER
+			coordX = 0
+			coordY = 0
+			return Action(action, coordX, coordY)
+
+		# LEAVE if we meet the game condition	
+		if self.coveredTiles == self.totalTiles:
                         return Action(AI.Action.LEAVE)
-		########################################################################
-		#							YOUR CODE ENDS							   #
-		########################################################################
+
+
+
+
+
